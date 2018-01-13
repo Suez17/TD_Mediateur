f9631ff5e9b45505d714edefe8777b828d20d8ee
Michaël <k-michael@hotmail.fr>
2018-01-13 23:38:07 +0100

    Push 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) {
         Mediator M = new Mediator();
         System.out.println(M.getResult("SELECT * FROM Etudiant"));
     }
